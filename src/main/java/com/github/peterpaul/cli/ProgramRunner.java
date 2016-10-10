c831c04ef58518db1a0ecd00c6d1f260c37baa43
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2016-10-10 10:07:43 +0200

    Refactor extract CommandRunner from ProgramRunner.

--- src/main/java/com/github/peterpaul/cli/ProgramRunner.java
+++ src/main/java/com/github/peterpaul/cli/ProgramRunner.java
@@ -1,12 +1,8 @@
 package com.github.peterpaul.cli;
 
-import com.github.peterpaul.cli.collection.ListUtil;
-import com.github.peterpaul.cli.collection.TooManyElementException;
 import com.github.peterpaul.cli.parser.ValueParser;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,51 +13,7 @@ public class ProgramRunner {
     public void run(String[] arguments, Object command) {
         parseOptions(arguments, command);
         parseArguments(arguments, command);
-
-        Optional<Method> runMethod = Stream.of(getRunMethodByAnnotation(command), getRunMethodByName(command))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .findFirst();
-        runMethod.ifPresent(method -> {
-            if (method.getParameterCount() != 0) {
-                throw new IllegalRunMethodException("Run method should not take any arguments, however '" + method.getName() + "' has '" + method.getParameterCount() + "'");
-            }
-            if (!method.getReturnType().equals(Void.TYPE)) {
-                throw new IllegalRunMethodException("Run method should not return anything, however '" + method.getName() + "' returns '" + method.getReturnType().getCanonicalName() + "'");
-            }
-        });
-        if (runMethod.isPresent()) {
-            try {
-                runMethod.get().invoke(command);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            throw new IllegalRunMethodException("No run method found.");
-        }
-    }
-
-    private Optional<Method> getRunMethodByName(Object command) {
-        Class<?> commandClass = command.getClass();
-        try {
-            Method method = commandClass.getMethod("run");
-            return Optional.of(method);
-        } catch (NoSuchMethodException e) {
-            return Optional.empty();
-        }
-    }
-
-    private Optional<Method> getRunMethodByAnnotation(Object command) {
-        Class<?> commandClass = command.getClass();
-        List<Method> annotatedRunMethods = Arrays.stream(commandClass.getDeclaredMethods())
-                .filter(m -> m.isAnnotationPresent(Cli.Run.class))
-                .collect(Collectors.toList());
-        try {
-            return ListUtil.tryGetUnique(annotatedRunMethods);
-        } catch (TooManyElementException e) {
-            throw new IllegalRunMethodException("Only one method may be annotated with " + Cli.Run.class.getCanonicalName(),
-                    e);
-        }
+        CommandRunner.runCommand(command);
     }
 
     private void parseArguments(String[] arguments, Object command) {
