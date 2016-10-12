c71fae5a19e25a48f4452a62455a0cb544c94976
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2016-10-12 21:41:07 +0200

    Load ValueParsers with ServiceLoader.

--- src/main/java/com/github/peterpaul/cli/ValueParserProvider.java
+++ src/main/java/com/github/peterpaul/cli/ValueParserProvider.java
@@ -1,29 +1,26 @@
 package com.github.peterpaul.cli;
 
+import com.github.peterpaul.cli.collection.ServiceLoaderStreamer;
 import com.github.peterpaul.cli.exceptions.ValueParseException;
 import com.github.peterpaul.cli.fn.Pair;
-import com.github.peterpaul.cli.parser.*;
+import com.github.peterpaul.cli.fn.Supplier;
+import com.github.peterpaul.cli.parser.ValueParser;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static com.github.peterpaul.cli.instantiator.InstantiatorSupplier.INSTANTIATOR_SUPPLIER;
 
 public class ValueParserProvider {
-    private static final Map<Class, ValueParser> valueParserMap;
+    private static final Supplier<Map<Class, ValueParser>> VALUE_PARSER_MAP_SUPPLIER = ((Supplier<Map<Class, ValueParser>>) () -> ServiceLoaderStreamer.stream(ValueParser.class)
+            .flatMap(p -> Arrays.stream(p.getSupportedClasses()).map(c -> Pair.of(c, p)))
+            .collect(getPairMapCollector())).cache();
 
-    static {
-        valueParserMap = Stream.of(
-                new BooleanValueParser(),
-                new FileValueParser(),
-                new IntValueParser(),
-                new StringValueParser(),
-                new UrlValueParser())
-                .flatMap(p -> Arrays.stream(p.getSupportedClasses()).map(c -> Pair.of(c, p)))
-                .collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
+    private static Collector<Pair<Class, ValueParser>, ?, Map<Class, ValueParser>> getPairMapCollector() {
+        return Collectors.toMap(Pair::getLeft, Pair::getRight);
     }
 
     public static ValueParser getValueParser(Field field, Class<? extends ValueParser> parserClass) {
@@ -31,9 +28,9 @@ public class ValueParserProvider {
         if (parserClass != ValueParser.class) {
             valueParser = INSTANTIATOR_SUPPLIER.supply().instantiate(parserClass);
         } else {
-            valueParser = valueParserMap.get(field.getType());
+            valueParser = VALUE_PARSER_MAP_SUPPLIER.supply().get(field.getType());
             if (valueParser == null) {
-                throw new ValueParseException("No ValueParser registered for: '" + field.getDeclaringClass().getCanonicalName() + "::" + field.getName()+ ": "  + field.getType().getCanonicalName() +  "', please specify one using the 'parser' attribute of the Cli annnotation.");
+                throw new ValueParseException("No ValueParser registered for: '" + field.getDeclaringClass().getCanonicalName() + "::" + field.getName() + ": " + field.getType().getCanonicalName() + "', please specify one using the 'parser' attribute of the Cli annnotation.");
             }
         }
         return valueParser;
